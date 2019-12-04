a21d2d7b45c7c39aad4ca1acc27292a1edf7fd6c
Alexander Lyon <arlyon@me.com>
2019-12-04 03:50:14 +0000

    Update scene to include some obstacles

--- src/main.cpp
+++ src/main.cpp
@@ -31,13 +31,26 @@ int main() {
     initializeUI(window);
     initializeInput(window);
 
+    renderable fishModel = renderable("models/fish.obj", "shaders/vertex_fish.glsl", "shaders/fragment_party_fish.glsl");
 
-    renderable fishModel = renderable("models/fish.obj", "shaders/vertex_fish.glsl", "shaders/fragment_fish.glsl");
+    renderable cubeModel = renderable("models/cube.obj", "shaders/vertex_fish.glsl", "shaders/fragment_party_fish.glsl");
 
     auto cam = registry.create();
-    registry.assign<position>(cam, glm::vec3(0,0,5), glm::quatLookAt(glm::vec3(0,0,-1), glm::vec3(0,1,0))); // raised slightly, and moved back toward the screen, looking at the origin
+    registry.assign<position>(cam, glm::vec3(0,20,100), glm::quatLookAt(glm::vec3(0,0.2,-1), glm::vec3(0,1,0)));
     registry.assign<velocity>(cam, glm::vec3(0,0,0));
     registry.assign<camera>(cam, &settings.fov, window);
+
+    auto cube = registry.create();
+    registry.assign<position>(cube, glm::vec3(0,0,0), glm::quatLookAt(glm::vec3(0,0,-1), glm::vec3(0,1,0)));
+    registry.assign<renderable>(cube, cubeModel);
+
+    auto origin = registry.create();
+    registry.assign<position>(origin, glm::vec3(0,1.5,0), glm::quatLookAt(glm::vec3(0,0,-1), glm::vec3(0,1,0)));
+    registry.assign<renderable>(origin, cubeModel);
+
+    auto origin1 = registry.create();
+    registry.assign<position>(origin1, glm::vec3(0,-1.5,0), glm::quatLookAt(glm::vec3(0,0,-1), glm::vec3(0,1,0)));
+    registry.assign<renderable>(origin1, cubeModel);
 
     double currentTime;
     double deltaTime;
@@ -50,6 +63,7 @@ int main() {
         lastTime = currentTime;
 
         /* Run Systems */
+        fish_physics(registry, deltaTime);
         physics(registry, deltaTime);
         fish_population(registry, fishModel);
 		boids(registry);
